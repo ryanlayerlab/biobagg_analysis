5363249d722d3b4368545f95f37a7c3ec3be152e
kristen-schneider <kristen.schneider@colorado.edu>
2024-06-10 09:51:08 -0600

    chnaged colors and scales for dist plot

--- plotting/figure3_ancestry.py
+++ plotting/figure3_ancestry.py
@@ -163,10 +163,35 @@ def plot_ancestry_group_distributions(genosis_scores,
                                       colors,
                                       png_file):
 
+    alpha_value = 0.5
+
     # Create the combined figure
     combined_figure, axes = plt.subplots(5, 4, figsize=(20, 12), dpi=300)
-    alpha_value = 0.5
-    genosis_x_max = 1500
+
+    col1_xlim = (0, 4000)
+    col1_ylim = (0, 0.01)
+    col2_xlim = (0.95, 1)
+    col2_ylim = (0, 1600)
+    col3_xlim = (-0.1, 1)
+    col3_ylim = (0, 250)
+    col4_xlim = (-0.25, 0.3)
+    col4_ylim = (0, 80)
+
+    # set axes limits
+    for col_j in range(4):
+        for row_i in range(5):
+            if col_j == 0:
+                axes[row_i, col_j].set_xlim(col1_xlim)
+                axes[row_i, col_j].set_ylim(col1_ylim)
+            elif col_j == 1:
+                axes[row_i, col_j].set_xlim(col2_xlim)
+                axes[row_i, col_j].set_ylim(col2_ylim)
+            elif col_j == 2:
+                axes[row_i, col_j].set_xlim(col3_xlim)
+                axes[row_i, col_j].set_ylim(col3_ylim)
+            elif col_j == 3:
+                axes[row_i, col_j].set_xlim(col4_xlim)
+                axes[row_i, col_j].set_ylim(col4_ylim)
 
     # Plot the GenoSiS scores in first column
     for i, superpop in enumerate(genosis_scores.keys()):
@@ -183,11 +208,11 @@ def plot_ancestry_group_distributions(genosis_scores,
                     fill=True, alpha=alpha_value)
         axes[0, 0].set_title('GenoSiS Scores', fontsize=20, fontweight='bold')
         axes[i, 0].set_ylabel('Density')
-        axes[i, 0].set_xlim(0, genosis_x_max)
+        # axes[i, 0].set_xlim(0, genosis_x_max)
         axes[i, 0].spines['top'].set_visible(False)
         axes[i, 0].spines['right'].set_visible(False)
         legend = ['Subpopulation', 'Superpopulation', 'Outgroup']
-        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i], ec="k") for i in range(len(legend))]
+        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i]) for i in range(len(legend))]
         axes[i, 0].legend(handles, legend, loc='upper right', frameon=False)
 
     # Plot the plink DST scores in second column
@@ -208,7 +233,7 @@ def plot_ancestry_group_distributions(genosis_scores,
         axes[i, 1].spines['top'].set_visible(False)
         axes[i, 1].spines['right'].set_visible(False)
         legend = ['Subpopulation', 'Superpopulation', 'Outgroup']
-        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i], ec="k") for i in range(len(legend))]
+        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i]) for i in range(len(legend))]
         axes[i, 1].legend(handles, legend, loc='upper right', frameon=False)
 
     # Plot the plink pi-hat scores in third column
@@ -229,7 +254,7 @@ def plot_ancestry_group_distributions(genosis_scores,
         axes[i, 2].spines['top'].set_visible(False)
         axes[i, 2].spines['right'].set_visible(False)
         legend = ['Subpopulation', 'Superpopulation', 'Outgroup']
-        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i], ec="k") for i in range(len(legend))]
+        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i]) for i in range(len(legend))]
         axes[i, 2].legend(handles, legend, loc='upper right', frameon=False)
 
     # Plot the plink kinship scores in fourth column
@@ -250,7 +275,7 @@ def plot_ancestry_group_distributions(genosis_scores,
         axes[i, 3].spines['top'].set_visible(False)
         axes[i, 3].spines['right'].set_visible(False)
         legend = ['Subpopulation', 'Superpopulation', 'Outgroup']
-        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i], ec="k") for i in range(len(legend))]
+        handles = [plt.Rectangle((0, 0), 1, 1, color=colors_list[i]) for i in range(len(legend))]
         axes[i, 3].legend(handles, legend, loc='upper right', frameon=False)
 
     # shift plots down and to the right
