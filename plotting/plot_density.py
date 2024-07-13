5a25db0608d0e1d9568e89d00ea6dd46406cccfe
kristen-schneider <kristen.schneider@colorado.edu>
2024-07-13 08:16:21 -0700

    adding plot for r2 by density

--- plotting/plot_density.py
+++ plotting/plot_density.py
@@ -2,6 +2,8 @@ import argparse
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import os
+
+import numpy as np
 import seaborn as sns
 import scipy.stats as stats
 import statistics
@@ -46,8 +48,9 @@ def get_sample_distances(distance_file):
     enc_distances = defaultdict(dict)
     emb_distances = defaultdict(dict)
     with open(distance_file, 'r') as f:
+        header = f.readline()
         for line in f:
-            line = line.strip().split('\t')
+            line = line.strip().split()
             sampleA = line[0]
             sampleB = line[1]
             enc_dist = float(line[2])
@@ -55,6 +58,28 @@ def get_sample_distances(distance_file):
             enc_distances[sampleA][sampleB] = enc_dist
             emb_distances[sampleA][sampleB] = emb_dist
     return enc_distances, emb_distances
+
+def get_single_r2(enc_distances, emb_distances):
+    '''
+    Calculate the r^2 value for a single sample
+    @param enc_distances:
+    @param emb_distances:
+    @return:
+    '''
+
+    sample_r2 = {}
+    # compare one sample to all others and get the single samples r^2 value
+    for sampleA in enc_distances:
+        encoding_distances = []
+        embedding_distances = []
+        for sampleB in enc_distances[sampleA]:
+            encoding_distances.append(enc_distances[sampleA][sampleB])
+            embedding_distances.append(emb_distances[sampleA][sampleB])
+
+        r2 = stats.pearsonr(encoding_distances, embedding_distances)[0] ** 2
+        sample_r2[sampleA] = r2
+
+    return sample_r2
 
 def plot_density_by_ancestry(sample_densities,
                              sample_subpopulations,
@@ -70,7 +95,7 @@ def plot_density_by_ancestry(sample_densities,
     # title = ('Density by Ancestry'
     #          '\nChromosome ' + chrm + ', segments (0,' + str(len(sample_densities['HG00096_0'])) + ')')
     title = ('Density by Ancestry'
-             '\nChromosome ' + chrm + ', segment9')
+             '\nChromosome ' + chrm)
 
     fig, axes = plt.subplots(num_superpops, 1,
                              figsize=(6, 10),
@@ -252,7 +277,8 @@ def plot_density_by_segment(segment_densities,
     plt.savefig(fig_name)
 
 
-def plot_encoding_embedding(enc_distances, emb_distances,
+def plot_encoding_embedding(good_segment_enc, good_segment_emb,
+                            good_segments,
                             sample_subpopulations,
                             sub_to_super,
                             colors,
@@ -267,98 +293,66 @@ def plot_encoding_embedding(enc_distances, emb_distances,
                                 figsize=(6, 10),
                                 sharex=True, sharey=True,
                                 dpi=300)
+    # separate plot per segment
+    for seg_idx in good_segments:
+        for superpop in superpops:
+            # get sample IDs for this superpopulation
+            samples = [sample for sample, subpop in sample_subpopulations.items() if sub_to_super[subpop] == superpop]
+            encoding_distances = []
+            embedding_distances = []
+            for sampleA in samples:
+                for sampleB in samples:
+                    # hap_0 = sampleA + '_0'
+                    # hap_1 = sampleA + '_1'
+                    sampleA_0 = sampleA + '_0'
+                    sampleA_1 = sampleA + '_1'
+                    sampleB_0 = sampleB + '_0'
+                    sampleB_1 = sampleB + '_1'
 
-    for superpop in superpops:
-        # get sample IDs for this superpopulation
-        samples = [sample for sample, subpop in sample_subpopulations.items() if sub_to_super[subpop] == superpop]
-        encoding_distances = []
-        embedding_distances = []
-        for sampleA in samples:
-            for sampleB in samples:
-                # hap_0 = sampleA + '_0'
-                # hap_1 = sampleA + '_1'
-                sampleA_0 = sampleA + '_0'
-                sampleA_1 = sampleA + '_1'
-                sampleB_0 = sampleB + '_0'
-                sampleB_1 = sampleB + '_1'
+                    if sampleA_0 in good_segment_enc[seg_idx] and sampleB_0 in good_segment_enc[seg_idx][sampleA_0]:
+                        encoding_distances.append(good_segment_enc[seg_idx][sampleA_0][sampleB_0])
+                        embedding_distances.append(good_segment_emb[seg_idx][sampleA_0][sampleB_0])
+                    if sampleA_0 in good_segment_enc[seg_idx] and sampleB_1 in good_segment_enc[seg_idx][sampleA_0]:
+                        encoding_distances.append(good_segment_enc[seg_idx][sampleA_0][sampleB_1])
+                        embedding_distances.append(good_segment_emb[seg_idx][sampleA_0][sampleB_1])
+                    if sampleA_1 in good_segment_enc[seg_idx] and sampleB_0 in good_segment_enc[seg_idx][sampleA_1]:
+                        encoding_distances.append(good_segment_enc[seg_idx][sampleA_1][sampleB_0])
+                        embedding_distances.append(good_segment_emb[seg_idx][sampleA_1][sampleB_0])
+                    if sampleA_1 in good_segment_enc[seg_idx] and sampleB_1 in good_segment_enc[seg_idx][sampleA_1]:
+                        encoding_distances.append(good_segment_enc[seg_idx][sampleA_1][sampleB_1])
+                        embedding_distances.append(good_segment_emb[seg_idx][sampleA_1][sampleB_1])
 
-                if sampleA_0 in enc_distances and sampleB_0 in enc_distances[sampleA_0]:
-                    encoding_distances.append(enc_distances[sampleA_0][sampleB_0])
-                    embedding_distances.append(emb_distances[sampleA_0][sampleB_0])
-                if sampleA_0 in enc_distances and sampleB_1 in enc_distances[sampleA_0]:
-                    encoding_distances.append(enc_distances[sampleA_0][sampleB_1])
-                    embedding_distances.append(emb_distances[sampleA_0][sampleB_1])
-                if sampleA_1 in enc_distances and sampleB_0 in enc_distances[sampleA_1]:
-                    encoding_distances.append(enc_distances[sampleA_1][sampleB_0])
-                    embedding_distances.append(emb_distances[sampleA_1][sampleB_0])
-                if sampleA_1 in enc_distances and sampleB_1 in enc_distances[sampleA_1]:
-                    encoding_distances.append(enc_distances[sampleA_1][sampleB_1])
-                    embedding_distances.append(emb_distances[sampleA_1][sampleB_1])
+            if len(encoding_distances) == 0:
+                continue
+            if len(embedding_distances) == 0:
+                continue
 
-        if len(encoding_distances) == 0:
-            continue
-        if len(embedding_distances) == 0:
-            continue
+            ax = axes[superpops.index(superpop)]
+            ax.scatter(encoding_distances, embedding_distances, alpha=0.5, color=colors[superpop])
+            ax.set_title(superpop, fontsize=15, color=colors[superpop], fontweight='bold')
+            ax.set_xlabel('Encoding Distance')
+            ax.set_ylabel('Embedding Distance')
 
-        ax = axes[superpops.index(superpop)]
-        ax.scatter(encoding_distances, embedding_distances, alpha=0.5, color=colors[superpop])
-        ax.set_title(superpop, fontsize=15, color=colors[superpop], fontweight='bold')
-        ax.set_xlabel('Encoding Distance')
-        ax.set_ylabel('Embedding Distance')
-
-        # get r^2 values
-        r2 = stats.pearsonr(encoding_distances, embedding_distances)[0] ** 2
-        ax.text(0.2, 0.9, 'r^2 = ' + str(round(r2, 2)),
-                horizontalalignment='center',
-                verticalalignment='center',
-                transform=ax.transAxes,
-                fontsize=10,
-                fontweight='bold',
-                color='black',
-                bbox=dict(facecolor=colors[superpop], edgecolor='black', boxstyle='round,pad=0.5'))
+            # get r^2 values
+            r2 = np.corrcoef(encoding_distances, embedding_distances)[0, 1]
+            ax.text(0.2, 0.9, 'r^2 = ' + str(round(r2, 2)),
+                    horizontalalignment='center',
+                    verticalalignment='center',
+                    transform=ax.transAxes,
+                    fontsize=10,
+                    fontweight='bold',
+                    color='black',
+                    bbox=dict(facecolor=colors[superpop], edgecolor='black', boxstyle='round,pad=0.5'))
 
 
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
 
-    fig.suptitle('Encoding vs. Embedding Distances\nChromosome ' + chrm, fontsize=20, fontweight='bold')
-    fig_name = out + 'encoding_embedding_chrm' + chrm + '.png'
-    plt.tight_layout()
-    plt.savefig(fig_name)
-
-    # encoding_distances = []
-    # embedding_distances = []
-    #
-    # for sampleA in enc_distances:
-    #     for sampleB in enc_distances[sampleA]:
-    #         encoding_distances.append(enc_distances[sampleA][sampleB])
-    #         embedding_distances.append(emb_distances[sampleA][sampleB])
-    #
-    # # plot encoding vs. embedding distances
-    # fig, ax = plt.subplots(figsize=(10, 10), dpi=300)
-    # ax.scatter(encoding_distances, embedding_distances, alpha=0.5)
-    # ax.set_xlabel('Encoding Distance')
-    # ax.set_ylabel('Embedding Distance')
-    #
-    # ax.spines['top'].set_visible(False)
-    # ax.spines['right'].set_visible(False)
-    #
-    # # calculate r^2
-    # r2 = stats.pearsonr(encoding_distances, embedding_distances)[0] ** 2
-    # # add r^2 to plot with decimal places = 2
-    # ax.text(0.1, 0.9, 'r^2 = ' + str(round(r2, 2)),
-    #         horizontalalignment='center',
-    #         verticalalignment='center',
-    #         transform=ax.transAxes,
-    #         fontsize=15,
-    #         fontweight='bold')
-    #
-    # title = ('Encoding vs. Embedding Distances'
-    #             '\nChromosome ' + chrm)
-    # fig.suptitle(title, fontsize=20, fontweight='bold')
-    # fig_name = out + 'encoding_embedding_chrm' + chrm + '.png'
-    # plt.tight_layout()
-    # plt.savefig(fig_name)
+        fig.suptitle('Encoding vs. Embedding Distances\nChromosome ' + chrm + ', segment ' + str(seg_idx),
+                     fontsize=20, fontweight='bold')
+        fig_name = out + 'encoding_embedding_chrm' + chrm + '_seg' + str(seg_idx) + '.png'
+        plt.tight_layout()
+        plt.savefig(fig_name)
 
 def plot_eur_afr_density(sample_densities,
                          sample_subpopulations,
@@ -435,6 +429,67 @@ def plot_eur_afr_density(sample_densities,
     plt.tight_layout()
     plt.savefig(fig_name)
 
+def plot_r2_density(good_segment_r2,
+                    sample_densities,
+                    good_segments,
+                    sample_subpopulations,
+                    sub_to_super,
+                    colors,
+                    chrm,
+                    out):
+    # plot r2 vs. density by ancestry
+    # x = density
+    # y = r2
+
+    superpops = sorted(set(sub_to_super.values()))
+    num_superpops = len(superpops)
+
+    for seg_idx in good_segments:
+        fig, axes = plt.subplots(num_superpops, 1,
+                                figsize=(6, 10),
+                                sharex=True, sharey=True,
+                                dpi=300)
+
+        for superpop in superpops:
+            # get sample IDs for this superpopulation
+            samples = [sample for sample, subpop in sample_subpopulations.items() if sub_to_super[subpop] == superpop]
+            densities = []
+            r2_values = []
+            for sample in samples:
+                # haplotypes
+                sample_0 = sample + '_0'
+                sample_1 = sample + '_1'
+                try:
+                    r2_values.append(good_segment_r2[seg_idx][sample_1])
+                    r2_values.append(good_segment_r2[seg_idx][sample_0])
+                    densities.append(sample_densities[sample_1][seg_idx])
+                    densities.append(sample_densities[sample_0][seg_idx])
+                except KeyError:
+                    continue
+
+            if len(densities) == 0:
+                continue
+            if len(r2_values) == 0:
+                continue
+
+            ax = axes[superpops.index(superpop)]
+            ax.scatter(densities, r2_values, alpha=0.5, color=colors[superpop])
+            ax.set_title(superpop, fontsize=15, color=colors[superpop], fontweight='bold')
+            ax.set_xlabel('Density')
+            ax.set_ylabel('r^2')
+
+            # y scale 0-1
+            ax.set_ylim(0, 1)
+
+            ax.spines['top'].set_visible(False)
+            ax.spines['right'].set_visible(False)
+
+        fig.suptitle('r^2 vs. Density\nChromosome ' + chrm + ', segment ' + str(seg_idx),
+                     fontsize=20, fontweight='bold')
+
+        fig_name = out + 'r2_density_chrm' + chrm + '_seg' + str(seg_idx) + '.png'
+        plt.tight_layout()
+        plt.savefig(fig_name)
 
 
 def read_colors(color_file):
@@ -459,7 +514,8 @@ def main():
     colors = read_colors(color_files)
 
     # read density files
-    chroms = [str(i) for i in range(8, 10)]
+    chroms = [str(i) for i in range(8, 9)]
+    good_segments = [81, 97]
 
     num_samples = 6406
 
@@ -468,11 +524,14 @@ def main():
         max_segments = 300
         sample_densities = defaultdict(lambda: [0] * max_segments)
         segment_densities = defaultdict(list)
+        good_segment_enc = {gs: defaultdict(dict) for gs in good_segments}
+        good_segment_emb = {gs: defaultdict(dict) for gs in good_segments}
+        good_segment_r2 = {gs: defaultdict(dict) for gs in good_segments}
 
         # open chromosome directory for densities
         dens_chrm_dir = density_dir + 'chrm' + chrm + '_dens/'
         for density_file in os.listdir(dens_chrm_dir):
-            if 'segment9' in density_file:
+            if 'segment' in density_file:
                 sample_densities = get_sample_densities(dens_chrm_dir, density_file, sample_densities)
                 segment_densities = get_segment_densities(dens_chrm_dir, density_file, segment_densities)
                 num_segments += 1
@@ -483,8 +542,14 @@ def main():
         # open chrm_dir for distances
         dist_chrm_dir = distance_dir + 'chrm' + chrm + '_dist/'
         for distance_file in os.listdir(dist_chrm_dir):
-            if 'segment81' in distance_file:
+            if 'segment' in distance_file:
+                seg_idx = int(distance_file.split('.')[1].replace('segment', ''))
                 enc_distances, emb_distances = get_sample_distances(dist_chrm_dir + distance_file)
+                sample_r2 = get_single_r2(enc_distances, emb_distances)
+                good_segment_enc[seg_idx] = enc_distances
+                good_segment_emb[seg_idx] = emb_distances
+                good_segment_r2[seg_idx] = sample_r2
+
             else:
                 # ignore
                 continue
@@ -492,22 +557,22 @@ def main():
 
         # resize sample densities to be length of num_segments
         for sample, densities in sample_densities.items():
-            # sample_densities[sample] = densities[:num_segments]
-            sample_densities[sample] = [densities[9]]
+            sample_densities[sample] = densities[:num_segments]
+            # sample_densities[sample] = [densities[136]]
 
         # resize segment densities to be length of num_segments
         for segment, densities in segment_densities.items():
             segment_densities[segment] = densities[:num_samples]
 
-
         # plot densities
-        print('Plotting densities by ancestry...', chrm)
-        plot_density_by_ancestry(sample_densities,
-                                 sample_subpopulations,
-                                 sub_to_super,
-                                 colors,
-                                 chrm,
-                                 out)
+
+        # print('Plotting densities by ancestry...', chrm)
+        # plot_density_by_ancestry(sample_densities,
+        #                          sample_subpopulations,
+        #                          sub_to_super,
+        #                          colors,
+        #                          chrm,
+        #                          out)
 
         # print('Plotting densities by segment...', chrm)
         # plot_density_by_segment(segment_densities,
@@ -518,8 +583,9 @@ def main():
         #                         chrm,
         #                         out)
 
-        # # print('Plotting density by r^2...', chrm)
-        # plot_encoding_embedding(enc_distances, emb_distances,
+        # print('Plotting encoding by embedding distances...', chrm)
+        # plot_encoding_embedding(good_segment_enc, good_segment_emb,
+        #                         good_segments,
         #                         sample_subpopulations,
         #                         sub_to_super,
         #                         colors,
@@ -532,6 +598,19 @@ def main():
         #                      sub_to_super,
         #                      chrm,
         #                      out)
+
+        print('Plotting r2 by density...', chrm)
+        plot_r2_density(good_segment_r2,
+                        sample_densities,
+                        good_segments,
+                        sample_subpopulations,
+                        sub_to_super,
+                        colors,
+                        chrm,
+                        out)
+
+
+
 
 
 
